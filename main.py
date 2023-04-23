decfb28b37e4f24f202c939ba3ad0048842f9871
wilfees <tenkovskaya_m@mail.ru>
2023-04-24 02:32:26 +0700

    теньковская мария: добавила проверку теста3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import forma as forma
 from flask import Flask, render_template, request, make_response, session
 from data import db_session, users
 from data.lesson import Lessons
@@ -123,7 +124,95 @@ def test1():
         db_sess.commit()
     return render_template('test1.html', form=forma)
 
+@app.route("/test2", methods=['GET', 'POST'])
 
+def test2():
+    name = 'Colors'
+    b = [0] * 9
+    if request.method == 'POST':
+        db_sess = db_session.create_session()
+        amount0 = request.form.get('Select0')
+        if amount0 == 'banana':
+            b[0] = 1
+        amount1 = request.form.get('Select1')
+        if amount1 == 'apple':
+            b[1] = 1
+        amount2 = request.form.get('Select2')
+        if amount2 == 'plum':
+            b[2] = 1
+        amount3 = request.form.get('Select3')
+        if amount3 == 'peach':
+            b[3] = 1
+        amount4 = request.form.get('Select4')
+        if amount4 == 'pear':
+            b[4] = 1
+        amount5 = request.form.get('Select5')
+        if amount5 == 'cucumber':
+            b[5] = 1
+        amount6 = request.form.get('Select6')
+        if amount6 == 'tomato':
+            b[6] = 1
+        amount7 = request.form.get('Select7')
+        if amount7 == 'potato':
+            b[7] = 1
+        amount8 = request.form.get('Select8')
+        if amount8 == 'orange':
+            b[8] = 1
+        result = sum(b)
+        user = db_sess.query(User.id)
+        les = Lessons(
+            lesson=name,
+            result=result,
+            user_id=user
+        )
+        db_sess.add(les)
+        db_sess.commit()
+    return render_template('test1.html', form=forma)
+
+@app.route("/test3", methods=['GET', 'POST'])
+
+def test3():
+    name = 'Animals'
+    b = [0] * 9
+    if request.method == 'POST':
+        db_sess = db_session.create_session()
+        amount0 = request.form.get('Select0')
+        if amount0 == 'cat':
+            b[0] = 1
+        amount1 = request.form.get('Select1')
+        if amount1 == 'dog':
+            b[1] = 1
+        amount2 = request.form.get('Select2')
+        if amount2 == 'bird':
+            b[2] = 1
+        amount3 = request.form.get('Select3')
+        if amount3 == 'rabbit':
+            b[3] = 1
+        amount4 = request.form.get('Select4')
+        if amount4 == 'fox':
+            b[4] = 1
+        amount5 = request.form.get('Select5')
+        if amount5 == 'monkey':
+            b[5] = 1
+        amount6 = request.form.get('Select6')
+        if amount6 == 'pig':
+            b[6] = 1
+        amount7 = request.form.get('Select7')
+        if amount7 == 'lion':
+            b[7] = 1
+        amount8 = request.form.get('Select8')
+        if amount8 == 'elephant':
+            b[8] = 1
+        result = sum(b)
+        user = db_sess.query(User.id)
+        les = Lessons(
+            lesson=name,
+            result=result,
+            user_id=user
+        )
+        db_sess.add(les)
+        db_sess.commit()
+    return render_template('test3.html', form=forma)
 
 @app.route('/account')
 def account():

b244b294a4c1fea73a80f7b671b41034ae4f0b68
wilfees <tenkovskaya_m@mail.ru>
2023-04-24 03:38:00 +0700

    теньковская мария: добавила проверку теста5

--- main.py
+++ main.py
@@ -259,6 +259,51 @@ def test4():
         db_sess.commit()
     return render_template('test4.html', form=forma)
 
+@app.route("/test5", methods=['GET', 'POST'])
+
+def test5():
+    name = 'Family'
+    b = [0] * 9
+    if request.method == 'POST':
+        db_sess = db_session.create_session()
+        amount0 = request.form.get('Select0')
+        if amount0 == 'kin':
+            b[0] = 1
+        amount1 = request.form.get('Select1')
+        if amount1 == 'mother':
+            b[1] = 1
+        amount2 = request.form.get('Select2')
+        if amount2 == 'father':
+            b[2] = 1
+        amount3 = request.form.get('Select3')
+        if amount3 == 'daughter':
+            b[3] = 1
+        amount4 = request.form.get('Select4')
+        if amount4 == 'son':
+            b[4] = 1
+        amount5 = request.form.get('Select5')
+        if amount5 == 'grandmother':
+            b[5] = 1
+        amount6 = request.form.get('Select6')
+        if amount6 == 'grandfather':
+            b[6] = 1
+        amount7 = request.form.get('Select7')
+        if amount7 == 'sister':
+            b[7] = 1
+        amount8 = request.form.get('Select8')
+        if amount8 == 'brother':
+            b[8] = 1
+        result = sum(b)
+        user = db_sess.query(User.id)
+        les = Lessons(
+            lesson=name,
+            result=result,
+            user_id=user
+        )
+        db_sess.add(les)
+        db_sess.commit()
+    return render_template('test5.html', form=forma)
+
 
 @app.route('/account')
 def account():

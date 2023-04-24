37a030b09a19350ad0fa849eade995680b4e4500
wilfees <tenkovskaya_m@mail.ru>
2023-04-24 16:12:16 +0700

    теньковская мария: проверка тестов

--- main.py
+++ main.py
@@ -130,6 +130,8 @@ def test1():
 
 @app.route("/test2", methods=['GET', 'POST'])
 def test2():
+    global result
+    global idis
     name = 'Colors'
     b = [0] * 9
     if request.method == 'POST':
@@ -161,24 +163,22 @@ def test2():
         amount8 = request.form.get('Select8')
         if amount8 == 'orange':
             b[8] = 1
-        result = sum(b)
-        user = db_sess.query(User).first()
-        user1 = user.id
-        db_sess.refresh()
-        les = Lessons(
-            lesson=name,
-            result=result,
-            user_id=user
-        )
-        db_sess.add(les)
-        db_sess.commit()
-        f = 4
-        redirect("/index.html", title='illustra', message=f)
-    return render_template('test1.html')
+            result = sum(b)
+            les = Lessons(
+                lesson=name,
+                result=result,
+                user_id=idis
+            )
+            db_sess.add(les)
+            db_sess.commit()
+            return render_template('results.html', message=result)
+        return render_template('test2.html')
 
 
 @app.route("/test3", methods=['GET', 'POST'])
 def test3():
+    global result
+    global idis
     name = 'Animals'
     b = [0] * 9
     if request.method == 'POST':
@@ -210,17 +210,159 @@ def test3():
         amount8 = request.form.get('Select8')
         if amount8 == 'elephant':
             b[8] = 1
-        result = sum(b)
-        user = db_sess.query(User).first()
-        user1 = user.id
-        les = Lessons(
-            lesson=name,
-            result=result,
-            user_id=user1
-        )
-        db_sess.add(les)
-        db_sess.commit()
-    return render_template('test3.html')
+            result = sum(b)
+            les = Lessons(
+                lesson=name,
+                result=result,
+                user_id=idis
+            )
+            db_sess.add(les)
+            db_sess.commit()
+            return render_template('results.html', message=result)
+        return render_template('test3.html')
+
+@app.route("/test4", methods=['GET', 'POST'])
+
+def test4():
+    global result
+    global idis
+    name = 'Numbers'
+    b = [0] * 9
+    if request.method == 'POST':
+        db_sess = db_session.create_session()
+        amount0 = request.form.get('Select0')
+        if amount0 == 'four':
+            b[0] = 1
+        amount1 = request.form.get('Select1')
+        if amount1 == 'two':
+            b[1] = 1
+        amount2 = request.form.get('Select2')
+        if amount2 == 'five':
+            b[2] = 1
+        amount3 = request.form.get('Select3')
+        if amount3 == 'seven':
+            b[3] = 1
+        amount4 = request.form.get('Select4')
+        if amount4 == 'nine':
+            b[4] = 1
+        amount5 = request.form.get('Select5')
+        if amount5 == 'one':
+            b[5] = 1
+        amount6 = request.form.get('Select6')
+        if amount6 == 'eight':
+            b[6] = 1
+        amount7 = request.form.get('Select7')
+        if amount7 == 'six':
+            b[7] = 1
+        amount8 = request.form.get('Select8')
+        if amount8 == 'three':
+            b[8] = 1
+            result = sum(b)
+            les = Lessons(
+                lesson=name,
+                result=result,
+                user_id=idis
+            )
+            db_sess.add(les)
+            db_sess.commit()
+            return render_template('results.html', message=result)
+        return render_template('test4.html')
+
+
+@app.route("/test5", methods=['GET', 'POST'])
+
+def test5():
+    global result
+    global idis
+    name = 'Family'
+    b = [0] * 9
+    if request.method == 'POST':
+        db_sess = db_session.create_session()
+        amount0 = request.form.get('Select0')
+        if amount0 == 'kin':
+            b[0] = 1
+        amount1 = request.form.get('Select1')
+        if amount1 == 'mother':
+            b[1] = 1
+        amount2 = request.form.get('Select2')
+        if amount2 == 'father':
+            b[2] = 1
+        amount3 = request.form.get('Select3')
+        if amount3 == 'daughter':
+            b[3] = 1
+        amount4 = request.form.get('Select4')
+        if amount4 == 'son':
+            b[4] = 1
+        amount5 = request.form.get('Select5')
+        if amount5 == 'grandmother':
+            b[5] = 1
+        amount6 = request.form.get('Select6')
+        if amount6 == 'grandfather':
+            b[6] = 1
+        amount7 = request.form.get('Select7')
+        if amount7 == 'sister':
+            b[7] = 1
+        amount8 = request.form.get('Select8')
+        if amount8 == 'brother':
+            b[8] = 1
+            result = sum(b)
+            les = Lessons(
+                lesson=name,
+                result=result,
+                user_id=idis
+            )
+            db_sess.add(les)
+            db_sess.commit()
+            return render_template('results.html', message=result)
+        return render_template('test5.html')
+
+@app.route("/test6", methods=['GET', 'POST'])
+
+def test6():
+    global result
+    global idis
+    name = 'Drinks'
+    b = [0] * 9
+    if request.method == 'POST':
+        db_sess = db_session.create_session()
+        amount0 = request.form.get('Select0')
+        if amount0 == 'water':
+            b[0] = 1
+        amount1 = request.form.get('Select1')
+        if amount1 == 'milkshake':
+            b[1] = 1
+        amount2 = request.form.get('Select2')
+        if amount2 == 'juice':
+            b[2] = 1
+        amount3 = request.form.get('Select3')
+        if amount3 == 'milk':
+            b[3] = 1
+        amount4 = request.form.get('Select4')
+        if amount4 == 'coffee':
+            b[4] = 1
+        amount5 = request.form.get('Select5')
+        if amount5 == 'cola':
+            b[5] = 1
+        amount6 = request.form.get('Select6')
+        if amount6 == 'lemonade':
+            b[6] = 1
+        amount7 = request.form.get('Select7')
+        if amount7 == 'tea':
+            b[7] = 1
+        amount8 = request.form.get('Select8')
+        if amount8 == 'cocoa':
+            b[8] = 1
+            result = sum(b)
+            les = Lessons(
+                lesson=name,
+                result=result,
+                user_id=idis
+            )
+            db_sess.add(les)
+            db_sess.commit()
+            return render_template('results.html', message=result)
+        return render_template('test6.html')
+
 
 
 @app.route('/account')

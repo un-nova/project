c890354f1b2bd79bcc63cb730a788ae49094ce68
wilfees <118731666+wilfees@users.noreply.github.com>
2023-04-30 23:58:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -574,5 +574,5 @@ def logout():
 db_session.global_init("db/blogs.db")
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
